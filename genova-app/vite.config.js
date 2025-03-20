0c642e46a994eb5fae79141bfdf07afd3a2c1bd2
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-20 00:07:34 +0000

    edited vercel.json & vite config file

--- genova-app/vite.config.js
+++ genova-app/vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./", // Ensure relative paths
   build: {
-    sourcemap: true, // Enable source maps
-  },
-  server: {
-    port: 5173,
+    outDir: "dist", // Make sure the output directory is correct
   }
-})
+});
