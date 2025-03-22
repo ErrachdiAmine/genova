b1351c9fd33395ec4567d4f44e069457d68f357d
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-22 23:16:58 +0000

    Add context to posts view endpoint

--- genova/api/views.py
+++ genova/api/views.py
@@ -32,7 +32,7 @@ class UserView(APIView):
 
     # --- POST: Create a new user (open to all, e.g., registration) ---
     def post(self, request):
-        serializer = UserSerializer(data=request.data, context= {'request': request})
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -71,7 +71,7 @@ class PostsView(APIView):
 
     # --- POST: Create a new post (requires auth) ---
     def post(self, request):
-        serializer = PostSerializer(data=request.data)
+        serializer = PostSerializer(data=request.data, context= {'request': request})
         if serializer.is_valid():
             serializer.save()  
             return Response(serializer.data, status=status.HTTP_201_CREATED)
