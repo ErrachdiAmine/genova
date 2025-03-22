1f5bcd3febbb281b804dc4079b6e88325bdf1cbf
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-22 01:41:08 +0000

    modified api views

--- genova/api/urls.py
+++ genova/api/urls.py
@@ -6,8 +6,9 @@ from .views import UserView, UserDetailsView, PostsView, check_login_status
 
 urlpatterns = [
   path('users/', UserView.as_view(), name='users'),
-  path('users/<int:pk>/', UserDetailsView.as_view(), name='user-detail'),
+  path('users/<int:pk>/', UserView.as_view(), name='user-detail'),
   path('posts/', PostsView.as_view(), name='posts'),
+  path('posts/<int:pk>/', PostsView.as_view(), name='post-detail'),
   path('check_login_status/', check_login_status, name='check_login_status'),
   
 ]
