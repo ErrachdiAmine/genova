d8d5353852ca5df55918183beed613eec2b6faca
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-22 23:04:55 +0000

    Alter posts's model author field settings

--- genova/api/views.py
+++ genova/api/views.py
@@ -73,7 +73,7 @@ class PostsView(APIView):
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Associate post with authenticated user
+            serializer.save()  
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
--- genova/core/models.py
+++ genova/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -35,7 +36,7 @@ class User(AbstractUser):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     
