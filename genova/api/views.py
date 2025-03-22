48e14b330c4cd79c7e01d002ec4c707c836f5504
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-22 22:50:52 +0000

    attach author automatically in posts serializer

--- genova/api/views.py
+++ genova/api/views.py
@@ -73,7 +73,7 @@ class PostsView(APIView):
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(author=request.user.username)  # Associate post with authenticated user
+            serializer.save()  # Associate post with authenticated user
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
