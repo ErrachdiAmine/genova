4e0863f9c2e735a49545e92ff54dd633d4dd5dcb
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-23 13:26:13 +0000

    Minor changes

--- genova-app/src/pages/Home.jsx
+++ genova-app/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
           to="/posts"
           className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition duration-300"
         >
-          Start here!
+          Explore Posts!
         </Link>
       </div>
     </div>
