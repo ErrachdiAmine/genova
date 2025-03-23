b358aa07c3bd42b99902223fc8780d342bc75281
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-23 03:26:38 +0000

    fixed login page UI problems

--- genova-app/src/pages/Login.jsx
+++ genova-app/src/pages/Login.jsx
@@ -51,8 +51,8 @@ const Login = () => {
   }, []);
 
   const logout = () => {
-    setIsLoadingUser(false);
     logoutUser(); // Use the logoutUser function
+    setIsLoadingUser(false);
     setCurrentUser(null);
   };
 
