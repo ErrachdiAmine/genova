65e49eaceefcff2dacffe659edac92728a419f79
ErrachdiAmine <errachdi.og@gmail.com>
2025-03-25 06:08:08 +0000

    edit post page functions

--- genova/api/views.py
+++ genova/api/views.py
@@ -114,3 +114,44 @@ class PostsView(APIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Post.DoesNotExist:
             return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+class Myposts(APIView) : 
+    authentication_classes = [JWTAuthentication]  # Always process JWT
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can access
+
+    def get(self, request):
+        posts = Post.objects.filter(author=request.user)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PostSerializer( data=request.data, context={'request': request} )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put (self, request, pk=None):
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(
+                post,
+                data=request.data,
+                context={'request': request}  # for CurrentUserDefault
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+    
+    def delete(self, request, pk=None):
+        try:
+            post = Post.objects.get(pk=pk)
+            post.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
