6b8a3672fcbd7cddde55181263ab38fddda85a29
Errachdi <errachdi.og@gmail.com>
2025-05-08 00:28:19 +0100

    Add migration to alter Post author field with related_name

--- genova/core/models.py
+++ genova/core/models.py
@@ -51,7 +51,7 @@ class Profile(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)    
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
